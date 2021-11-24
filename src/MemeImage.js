4bea2fd6f9fb83fcb38cf9ecffb10f840c83eb92
Yulerik Sorensen <yulerik.sorensen@gmail.com>
2021-11-24 09:36:18 -0800

    changed MemeImage to a class component. added state. set state as array. Add randomImage function to button. replaces random image on screen with a new image from the array.

--- src/MemeImage.js
+++ src/MemeImage.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function MemeImage(props) {
-    console.log(props)
+class MemeImage extends React.Component {
     // gets random index for image
-    const imageUrl = props.props.length === 0 ? '' : props.props[Math.floor(Math.random() * (props.props.length - 0 + 1) + 0)].url
+    constructor() {
+        super()
+        this.state = {
+            memeObjects: []
 
-    return (
-        <div>
-            <img style={{height: '500px'}} alt='meme image' src={imageUrl}></img>
-            <button>Random Image</button>
-        </div>
-    )
+        }
+
+        this.randomImage = this.randomImage.bind(this)
+    }
+    componentDidMount() {
+        this.setState({memeObjects: this.props})
+    }
+    
+    randomImage(event) {
+        event.preventDefault()
+        console.log(this.state.memeObjects)
+        document.getElementById('meme-image').src = this.state.memeObjects.props[Math.floor(Math.random() * (this.state.memeObjects.props.length - 0 + 1) + 0)].url
+    }
+
+    render() {
+        const imageUrl = this.props.props.length === 0 ? '' : this.props.props[Math.floor(Math.random() * (this.props.props.length - 0 + 1) + 0)].url
+        return (
+            <div>
+                <img id='meme-image' style={{height: '500px'}} alt='meme image' src={imageUrl}></img>
+                <button onClick={this.randomImage}>Random Image</button>
+            </div>
+        )
+    }
 }
 
 export default MemeImage
